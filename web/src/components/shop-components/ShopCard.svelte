5e550afe5c0d653825b972b3b5752bf7c73720a0
masonschafercodes <indytechbox@gmail.com>
2023-01-15 06:24:25 -0500

    feat: check icon on add item to cart

--- web/src/components/shop-components/ShopCard.svelte
+++ web/src/components/shop-components/ShopCard.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
     export let item: any;
 
+  import { CheckCircle } from "phosphor-svelte";
     import { cartItems } from "../../store/stores";
 
+    let hasAdded = false;
+
     function addItem(item: any) {
+        hasAdded = true;
         cartItems.update((items) => {
             items.push(item);
             return items;
         });
+
+        setTimeout(() => {
+            hasAdded = false;
+        }, 1000);
     }
 </script>
 
@@ -15,5 +23,9 @@
     <h1 class="font-semibold leading-tight">{item.name}</h1>
     <p class="text-sm">{item.desc}</p>
     <p class="text-sm">{item.price}</p>
-    <button class="p-2 rounded-md border border-stone-200 bg-stone-700 text-stone-100 font-semibold hover:bg-stone-800" on:click={() => addItem(item)}>Add to Cart</button>
+    <button class="p-2 rounded-md border border-stone-200 bg-stone-700 text-stone-100 font-semibold hover:bg-stone-800 flex items-center justify-center gap-2" on:click={() => addItem(item)}>
+        {#if hasAdded}
+            <CheckCircle size={24} weight="fill" />
+        {/if}
+        Add to Cart</button>
 </div>
